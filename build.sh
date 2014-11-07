9e2703aeca6e160af31c690d0fd3ecc6c1eb4940
Hao Chen <aressera@yahoo-inc.com>
2014-11-06 21:37:02 -0800

    changed icon, added splash screen for iphone 6(+), modified build.sh

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ sencha app build native
 
 #copy the icon screen
 cp -r res/* phonegap/www/res/
+cp icon.png phonegap/www/
 
 #git commit & push
 cd phonegap ; git add -A ; git commit -m 'new commit' ; git push origin master
